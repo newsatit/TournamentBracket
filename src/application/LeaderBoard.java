ea081dbd1d41f10774e8153d6b95b29f80d57970
Neal Satitsumpun <satitsumpun@wisc.edu>
2018-05-02 00:01:19 -0500

    Add file header

--- src/application/LeaderBoard.java
+++ src/application/LeaderBoard.java
@@ -1,3 +1,14 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS400 Spring 2018
+// PROJECT:          A-Team TournamentBracket
+// FILES:            Bracket.java, Challenger.java, ChallengerBlock.java, Leaderboard.java, Main.java, Match.java, application.css
+//
+// USER:             Zhichun Huang, Neal Pongmorrakot, Summer Rawfert, Neal Satitsumpun, Andrew Schaefer
+//
+// Instructor:       Deb Deppeler (deppeler@cs.wisc.edu)
+// Bugs:             no known bugs
+//
+//////////////////////////// 80 columns wide //////////////////////////////////
 package application;
 
 import javafx.collections.ObservableList;
